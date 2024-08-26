fa790b4f3bd70e1fdf25469f67b4c0824d63ecc5
Renato <96966909+RenatoEnciso@users.noreply.github.com>
2024-08-26 14:55:59 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,27 @@
 import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from "@bot-whatsapp/bot";
 import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys';
 import cors from "cors"; // Importa el paquete cors
+import https from 'https';
+import fs from 'fs';
 
 const flowBienvenida = addKeyword('hola').addAnswer('Buenas! Bienvenido');
 
 const main = async () => {
     const provider = createProvider(BaileysProvider);
+
+    // Cargar certificados SSL en formato PEM
     const sslOptions = {
         key: fs.readFileSync('key.pem'),
         cert: fs.readFileSync('cert.pem')
     };
+
+    // Crear e iniciar el servidor HTTPS
     const httpsServer = https.createServer(sslOptions, provider.http?.server);
 
     httpsServer.listen(3200, () => {
         console.log('Servidor HTTPS ejecutándose en https://localhost:3200');
     });
-    
-     const httpsServer = https.createServer(sslOptions, provider.http?.server);
+
     // Configura CORS para aceptar todas las solicitudes
     provider.http?.server.use(cors({
         origin: '*', // Permite solicitudes de cualquier origen
@@ -24,6 +29,7 @@ const main = async () => {
         allowedHeaders: ['Content-Type', 'Authorization'], // Permite los encabezados especificados
     }));
 
+    // Configura la ruta para enviar mensajes
     provider.http?.server.post('/send-message', handleCtx(async (bot, req, res) => {
         try {
             const body = req.body;
@@ -37,15 +43,16 @@ const main = async () => {
                 media: mediaUrl
             });
 
-            res.end('Mensaje enviado correctamente!!!')
+            res.end('Mensaje enviado correctamente!!!');
         } catch (error) {
             console.error('Error al enviar el mensaje:', error);
-            res.end('Mensaje no enviado!!!')
+            res.end('Mensaje no enviado!!!');
         }
     }));
 
+    // Crear el bot
     await createBot({
-        flow: createFlow([]),
+        flow: createFlow([flowBienvenida]),
         database: new MemoryDB(),
         provider
     });
