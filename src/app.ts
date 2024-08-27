bce053532184f0e2d4964a3c000d6df299b5d60b
Jogaqui <96966786+Jogaqui@users.noreply.github.com>
2024-08-26 20:05:34 -0500

    pequeña modificacion

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,40 @@
 import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from "@bot-whatsapp/bot";
 import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys';
 import cors from "cors";
-import https from "https";
-import fs from "fs";
-
-// Certificados SSL/TLS
-const options = {
-    key: fs.readFileSync('key.pem'), 
-    cert: fs.readFileSync('cert.pem'), 
-};
+import https from 'https';
+import fs from 'fs';
+import express from 'express';
 
 const flowBienvenida = addKeyword('hola').addAnswer('Buenas! Bienvenido');
 
 const main = async () => {
-    const provider = createProvider(BaileysProvider);
-
-    const server = https.createServer(options, provider.http?.server);
-
-    // Configura CORS para aceptar todas las solicitudes
-    provider.http?.server.use(cors({
+    const app = express();
+    app.use(cors({
         origin: '*',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
     }));
 
-    provider.http?.server.post('/send-message', handleCtx(async (bot, req, res) => {
+    const httpsOptions = {
+        key: fs.readFileSync('key.pem'),
+        cert: fs.readFileSync('cert.pem')
+    };
+
+    const httpsServer = https.createServer(httpsOptions, app);
+
+    const provider = createProvider(BaileysProvider);
+
+    app.post('/send-message', handleCtx(async (bot, req, res) => {
         try {
             const body = req.body;
             const phone = body.phone;
             const message = body.message;
             const mediaUrl = body.mediaUrl;
-
             console.log(body);
             
             await bot.sendMessage(phone, message, {
                 media: mediaUrl
             });
-
             res.end('Mensaje enviado correctamente!!!')
         } catch (error) {
             console.error('Error al enviar el mensaje:', error);
@@ -44,17 +42,17 @@ const main = async () => {
         }
     }));
 
-    server.listen(3002, () => {
-        console.log('Servidor HTTPS funcionando en el puerto 3002');
+    httpsServer.listen(3002, () => {
+        console.log('Servidor HTTPS escuchando en el puerto 3002');
     });
 
     await createBot({
-        flow: createFlow([]),
+        flow: createFlow([flowBienvenida]),
         database: new MemoryDB(),
         provider
     });
 };
 
 main().catch(error => {
-    console.error('Error en la aplicación:', error);
+    console.error('Error en la aplicación:', error);
 });
